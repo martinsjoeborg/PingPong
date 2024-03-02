5b40de003660e271a4a5d482384b7fa64dd540df
martinsjoeborg <113284827+martinsjoeborg@users.noreply.github.com>
2024-03-02 14:04:00 +0100

    bot

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const sepoliaProvider = new ethers.JsonRpcProvider('https://sepolia.infura.io/v3
 const walletPrivateKey = 'fb738bc6491b81da284be3676bf5e914ee0fca27d0a0ffb26fee2bcac00ea0e6';
 const wallet = new ethers.Wallet(walletPrivateKey, sepoliaProvider);
 
-const sepoliaContractAddress = '0x68857672427c6F823F1f5e4D17ae4E149AB23018';
+const sepoliaContractAddress = '0x8db9B00eB649d4fF509285aF965E77AcFD551031';
 const sepoliaContract = new ethers.Contract(sepoliaContractAddress, CONTRACT_ABI, wallet);
 
 
@@ -15,6 +15,7 @@ let startBlock = 5388354;
 sepoliaContract.on('Ping', async (event) => {
     console.log(`Received Ping event from block: ${event.log.blockNumber}`);
     await sendPong(event.log.transactionHash, event.log.blockNumber);
+    // console.log(event.log.transactionHash);
 });
 
 async function sendPong(pingHash, blockNumber) {
@@ -26,11 +27,12 @@ async function sendPong(pingHash, blockNumber) {
 
     try {
         const tx = await sepoliaContract.pong(pingHash, { gasLimit: 2100000 });
-        console.log(`Sent Pong transaction: ${tx.hash}`, `StartBlock: ${startBlock}`);
+        console.log(`Sent Pong transaction with Pong txHash: ${tx.hash}`, `Ping txHash ${pingHash}`, `PingBlock: ${startBlock}`);
+        // console.log(pingHash);
     } catch (error) {
         console.error(`Failed to send Pong for block ${blockNumber}: ${error.message}`);
     }
     console.log(startBlock);
 }
 
-console.log(startBlock);
+// console.log(startBlock);
