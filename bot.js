1b5eaa08878925429fe94b9dcc80730cd89df011
martinsjoeborg <113284827+martinsjoeborg@users.noreply.github.com>
2024-03-08 02:37:24 +0100

    gas + nonce

--- bot.js
+++ bot.js
@@ -3,15 +3,15 @@ import { CONTRACT_ABI } from "./config.js";
 import fs from 'fs';
 import path from 'path';
 
-const sepoliaProvider = new ethers.JsonRpcProvider('https://sepolia.infura.io/v3/070c174a2442432a803e427d142d79c1');
-const walletPrivateKey = '63240b8b01f2a12eb06aa0f92299c5482ca73ac15761657881155ea9489022ed';
+const sepoliaProvider = new ethers.InfuraWebSocketProvider("sepolia", "070c174a2442432a803e427d142d79c1");
+const walletPrivateKey = '1cf4b1702f07d9090ab6c165f90564f3a4d516ddc0193740e19d846f9f59bf20';
 const wallet = new ethers.Wallet(walletPrivateKey, sepoliaProvider);
 
-const sepoliaContractAddress = '0x3481E00b8C053D2402B8ac4FBD0B73164Cf04b63';
+const sepoliaContractAddress = '0x8e36A56dB311222927b3aa8BEB5C3c8861320FEE';
 const sepoliaContract = new ethers.Contract(sepoliaContractAddress, CONTRACT_ABI, wallet);
 
-const nonce = await sepoliaProvider.getTransactionCount("0xbcAAd13349E231A21A0c4fd75746Cf8828B50206");
-// console.log(nonce);
+const nonce = await sepoliaProvider.getTransactionCount("0x7A009cF93A68533bf15347a9C6637C229c6d25BA");
+console.log(nonce);
 
 let startBlock = readLastState().lastBlock;
 
@@ -23,7 +23,6 @@ function readLastState() {
     }
     return { lastBlock: 0, nonce: 0 };
 }
-// console.log(readLastState().nonce);
 
 function updateLastState(blockNumber, nonce) {
     const filePath = path.join("./", 'lastState.json');
@@ -34,7 +33,6 @@ function updateLastState(blockNumber, nonce) {
 sepoliaContract.on('Ping', async (event) => {
     console.log(`Ping event from block: ${event.log.blockNumber}`);
     await sendPong(event.log.transactionHash, event.log.blockNumber);
-    // console.log(event.log.transactionHash);
 });
 
 async function sendPong(pingHash, blockNumber) {
@@ -45,10 +43,11 @@ async function sendPong(pingHash, blockNumber) {
     startBlock = blockNumber;
 
     try {
-        const nonce = readLastState().nonce;
-        const tx = await sepoliaContract.pong(pingHash, { gasLimit: 2100000 });
+        const nonce = await sepoliaProvider.getTransactionCount("0x7A009cF93A68533bf15347a9C6637C229c6d25BA");
+        const gasPrice = await sepoliaProvider.getFeeData();
+        const tx = await sepoliaContract.pong(pingHash, { gasPrice: gasPrice.maxFeePerGas, nonce: nonce});
         console.log(`Sent Pong transaction with Pong txHash: ${tx.hash}`, `Ping txHash ${pingHash}`, `PingBlock: ${startBlock}`);
-        updateLastState(blockNumber, nonce + 1);
+        updateLastState(blockNumber, nonce);
         console.log(tx);
     } catch (error) {
         console.error(`Failed to send Pong for block ${blockNumber}: ${error.message}`);
